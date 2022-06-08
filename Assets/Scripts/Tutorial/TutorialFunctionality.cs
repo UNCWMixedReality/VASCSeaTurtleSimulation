569025a8b7d5620e0be91aee1b11712794162b93
Austin Whittaker <abw1717@uncw.edu>
2022-06-08 13:35:50 -0400

    :Created an orb using particle system to gain user's attention to guide the user to the next room. Orb movement used using timeline sequencing. Chnaged the olors of the walls from egg shell. Moved the player back just a little bit so the screen isn't right in your face.

--- Assets/Scripts/Tutorial/TutorialFunctionality.cs
+++ Assets/Scripts/Tutorial/TutorialFunctionality.cs
@@ -3,23 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 //using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
 
 public class TutorialFunctionality : MonoBehaviour
 {
+
     public GameObject firstDoor;
     public GameObject secondDoor;
     public AudioSource doorA;
     public AudioSource doorB;
     public GameObject teleportInstructions;
     public GameObject snapInstructions;
+    public GameObject MoveRooms;
     public AudioFeedback audiofeedback;
     public AudioConvoTutorial ACT;
     public IMTutorial IMT;
+    public PlayableDirector director;
+    public PlayableDirector directorRm2;
+    
+
 
     private int stage = 0;
     private Vector2 touchPadL = new Vector2();
     private Vector2 touchPadR = new Vector2();
 
+
+    
     void FixedUpdate()
     {
         if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickUp) || OVRInput.Get(OVRInput.Button.PrimaryThumbstickLeft) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft))
@@ -48,19 +57,29 @@ public class TutorialFunctionality : MonoBehaviour
             //doorA.Play();
             ACT.playSound();
             IMT.changePanel(3);
+            displayMoveRoom2();
+
             stage++;
+            
             audiofeedback.playGood();
             Debug.Log("First Snap Turn");
             Debug.Log($"Stage: {stage}");
+            
+            
         }
+        
     }
 
     public void firstObjectGrab()
     {
+        
         audiofeedback.playSelection();
+        StopFirstTimeline();
         if (stage == 2)
         {
+            
             Destroy(secondDoor);
+            StartSecondTimeline();
             //doorB.Play();
             ACT.playSound();
             IMT.changePanel(5);
@@ -73,6 +92,7 @@ public class TutorialFunctionality : MonoBehaviour
 
     public void firstButtonClick()
     {
+        StopSecondTimeline();
         if (stage == 3)
         {
             audiofeedback.playGood();
@@ -90,4 +110,31 @@ public class TutorialFunctionality : MonoBehaviour
         ACT.playSound();
         IMT.changePanel(2);
     }
+    void displayMoveRoom2()
+    {
+        
+        teleportInstructions.SetActive(false);
+        snapInstructions.SetActive(false);
+        MoveRooms.SetActive(true);
+        StartFirstTimeline();
+
+    }
+    public void StartFirstTimeline()
+    {
+        director.Play();
+    }
+    public void StartSecondTimeline()
+    {
+        directorRm2.Play();
+    }
+    public void StopFirstTimeline()
+    {
+        director.Stop();
+    }
+    public void StopSecondTimeline()
+    {
+        directorRm2.Stop();
+    }
+
+
 }

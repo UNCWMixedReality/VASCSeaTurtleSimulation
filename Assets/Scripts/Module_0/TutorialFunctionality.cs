a9b215c918dd8c01d6c6aa7b1302837cbbd1ceea
NicholasBrunsink <njb4775@uncw.edu>
2022-06-27 13:15:36 -0400

    fixed primary stick right turn not progressing player in module 0

--- Assets/Scripts/Module_0/TutorialFunctionality.cs
+++ Assets/Scripts/Module_0/TutorialFunctionality.cs
@@ -22,7 +22,7 @@ public class TutorialFunctionality : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickUp) || OVRInput.Get(OVRInput.Button.PrimaryThumbstickLeft) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft))
+        if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickRight) || OVRInput.Get(OVRInput.Button.PrimaryThumbstickLeft) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft))
         {
             firstSnapTurn();
         }

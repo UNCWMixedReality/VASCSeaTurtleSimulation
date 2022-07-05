eed950a34d9c522c285729b055c09b38df7d370b
Brennon Treadwell <bjt1798@uncw.edu>
2022-07-05 09:34:05 -0400

    added files for m1 progression

---
+++ Assets/Scripts/Module_1/New/NewTaskManagerM1.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewTaskManagerM1 : MonoBehaviour
+{
+    //class variables
+    #region
+    public NewInstructionUpdaterM1 instrUpdater;
+    public NewActivityManagerM1 activityMan;
+
+    //number of tasks completed and when each task was completee
+    public int taskCount { get; set; }
+    public float[] taskTimes { get; set; }
+
+    //Table Manager Scripts
+    public NewToolManagerM1 toolTable;
+    public NewMeasuringManagerM1 measureTable;
+    public NewIdentificationManagerM1 identifyTable;
+    public NewTrackManagerM1 trackTable;
+    #endregion
+
+    public void MarkTaskCompletion(int taskID)
+    {
+        if(taskID != taskCount)
+        {
+            return;
+        }
+
+        //mark time and increment task count
+        taskTimes[taskCount] = Time.time;
+        taskCount += 1;
+        Debug.Log("The task count is, " + (taskCount));
+
+        //perform appropriate set up based on current task count
+        #region
+        if(taskCount == 1)
+        {
+            //
+        }
+        #endregion
+        //run the next set of instructions
+        //instrUpdater.RunInstructions();
+
+
+    }
+}

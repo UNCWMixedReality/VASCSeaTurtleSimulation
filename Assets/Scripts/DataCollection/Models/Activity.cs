8a8345e364e05d9ced0372fccca2de7e9bf6f77f
Brennon Treadwell <bjt1798@uncw.edu>
2022-07-12 12:51:06 -0400

    fixed scene logging

--- Assets/Scripts/DataCollection/Models/Activity.cs
+++ Assets/Scripts/DataCollection/Models/Activity.cs
@@ -8,7 +8,6 @@ namespace DataCollection.Models
     {
         public static int ActivityCount;
         public string Id { get; }
-        public string Scene { get; }
         public DateTime Timestamp { get; }
         public string ActivityDetail { get; }
         
@@ -16,7 +15,6 @@ namespace DataCollection.Models
         public Activity(string id, string scene, DateTime timestamp, string activityDetail)
         {
             Id = id;
-            Scene = scene;
             Timestamp = timestamp;
             ActivityDetail = activityDetail;
             ActivityCount++;
@@ -24,7 +22,6 @@ namespace DataCollection.Models
         public Activity(DateTime timestamp, string scene, string activityDetail)  // First Attempt at what might be needed for activity log
         {
             Id = (ActivityCount++).ToString();
-            Scene = scene;
             Timestamp = timestamp;
             ActivityDetail = activityDetail;
         }

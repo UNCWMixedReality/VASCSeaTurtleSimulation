8d0d1dc9e9ea44ba091fcca2f8b0062b3ca1dc1f
blakebiz <blakeblackport@gmail.com>
2022-09-09 14:47:09 -0400

    reverted last 2 commits

---
+++ Assets/Scripts/DataCollection/SetSessionScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DataCollection;
+
+
+namespace DataCollection
+{
+    public class SetSessionScene : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            DcDataLogging.Session.SessionScene = SceneManager.GetActiveScene().name;
+        }
+    }
+}
+

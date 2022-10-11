a5d078d696ed8841205a841f30bfa8051787ee60
NicholasBrunsink <njb4775@uncw.edu>
2022-10-11 15:55:36 -0400

    egg sort is mostly working. need to figure out kinks with progression. Gloves and egg sorting is done though

---
+++ Assets/Scripts/UltimateXR custom scripts/EggSortManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EggSortManager : MonoBehaviour
+{
+    //public TaskManager taskMan;
+
+    // Text objects for counting eggs sorted
+    public Text goodEggText;
+    public Text badEggText;
+
+    // base number of good/bad eggs in scene
+    public int numGoodEggs;
+    public int numBadEggs;
+
+    // keeps track of total num eggs sorted
+    private int goodEggsDone;
+    private int badEggsDone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goodEggsDone = 0;
+        badEggsDone = 0;
+        goodEggText.text = "0";
+        badEggText.text = "0";
+    }
+
+    // increments the number of good eggs sorted then checks if all eggs are sorted
+    public void IncrementGoodEggs()
+    {
+        goodEggsDone++;
+        goodEggText.text = goodEggsDone.ToString();
+        CheckDone();
+    }
+
+    // increments number of bad eggs sorted and checks if all eggs are sorted
+    public void IncrementBadEggs()
+    {
+        badEggsDone++;
+        badEggText.text = badEggsDone.ToString();
+        CheckDone();
+    }
+
+    public void CheckDone()
+    {
+        if (goodEggsDone == numGoodEggs && badEggsDone == numBadEggs)
+        {
+            //taskMan.MarkTaskCompletion();
+            Debug.Log("sorting done");
+        }
+    }
+}

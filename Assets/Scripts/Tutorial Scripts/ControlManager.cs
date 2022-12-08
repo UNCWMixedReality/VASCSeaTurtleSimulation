31ac5ca7e5958f704a504f993a31d0c431b3109b
NicholasBrunsink <njb4775@uncw.edu>
2022-12-08 14:02:14 -0500

    finished computing showcase demo. Need to fix softlock in tutorial

--- Assets/Scripts/Tutorial Scripts/ControlManager.cs
+++ Assets/Scripts/Tutorial Scripts/ControlManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UltimateXR.Avatar;
 using UltimateXR.Devices;
+using UltimateXR.Core;
 
 
 public class ControlManager : MonoBehaviour
@@ -45,6 +46,27 @@ public class ControlManager : MonoBehaviour
             {
                 taskMan.MarkTaskCompletion(4);
             }
+        }
+    }
+
+    void Update()
+    {
+        if (UxrAvatar.LocalAvatarInput.GetButtonsPressDown(UxrHandSide.Left, UxrInputButtons.Button1) || UxrAvatar.LocalAvatarInput.GetButtonsPressDown(UxrHandSide.Right, UxrInputButtons.Button1))
+        {
+            taskMan.MarkTaskCompletion(1);
+        }
+        else if (UxrAvatar.LocalAvatarInput.GetButtonsPressDown(UxrHandSide.Left, UxrInputButtons.Button2) || UxrAvatar.LocalAvatarInput.GetButtonsPressDown(UxrHandSide.Right, UxrInputButtons.Button2))
+        {
+            taskMan.MarkTaskCompletion(2);
+        }
+        else if (UxrAvatar.LocalAvatarInput.GetButtonsPressDown(UxrHandSide.Left, UxrInputButtons.Grip) || UxrAvatar.LocalAvatarInput.GetButtonsPressDown(UxrHandSide.Right, UxrInputButtons.Grip))
+        {
+            taskMan.MarkTaskCompletion(3);
+        }
+        else if (UxrAvatar.LocalAvatarInput.GetButtonsPressDown(UxrHandSide.Left, UxrInputButtons.DPadLeft) || UxrAvatar.LocalAvatarInput.GetButtonsPressDown(UxrHandSide.Right, UxrInputButtons.DPadLeft) ||
+            UxrAvatar.LocalAvatarInput.GetButtonsPressDown(UxrHandSide.Left, UxrInputButtons.DPadRight) || UxrAvatar.LocalAvatarInput.GetButtonsPressDown(UxrHandSide.Right, UxrInputButtons.DPadRight))
+        {
+            taskMan.MarkTaskCompletion(4);
         }
     }
 }
